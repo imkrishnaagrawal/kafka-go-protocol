8a4b66f75f734db4ba0676836020fa5e72474648
Krishna Agrawal <imkrishnaagrawal@gmail.com>
2024-03-25 21:39:25 +0530

    refactor code

--- cmd/types/kafka.go
+++ cmd/types/kafka.go
@@ -74,20 +74,3 @@ func (kafka *Kafka) Read() (*bytes.Reader, error) {
 
 	return bytes.NewReader(responseBuf), nil
 }
-
-type RequestHeader struct {
-	APIKey        int16
-	APIVersion    int16
-	CorrelationID int32
-	ClientId      string
-}
-
-func (requestHeader *RequestHeader) Byte() []byte {
-	buf := bytes.NewBuffer([]byte{})
-	binary.Write(buf, binary.BigEndian, requestHeader.APIKey)
-	binary.Write(buf, binary.BigEndian, requestHeader.APIVersion)
-	binary.Write(buf, binary.BigEndian, requestHeader.CorrelationID)
-	binary.Write(buf, binary.BigEndian, uint16(len(requestHeader.ClientId)))
-	binary.Write(buf, binary.BigEndian, []byte(requestHeader.ClientId))
-	return buf.Bytes()
-}
